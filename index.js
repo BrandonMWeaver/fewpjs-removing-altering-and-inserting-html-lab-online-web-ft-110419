54f39d106ad6d65aa3dd5af9525e80deb806324f
Brandon Weaver <a7xncob@gmail.com>
2020-02-18 21:55:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "Brandon Weaver is the champion";
+document.body.append(newHeader);
