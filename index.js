16b5994b4e2c80b24635c228b05de632e0173b09
Brandon Weaver <a7xncob@gmail.com>
2020-02-18 21:53:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.querySelector("main#main").remove();
-let newHeader = document.createElement("h1#victory");
-document.body.append(newHeader);
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
